ff83b39809b8a70a90be9da4762f53adbc32f48c
YusufLisawi <yusufisawi@gmail.com>
2023-04-13 02:51:27 +0000

    Adding sessions feed

--- resources/js/Components/Badge.jsx
+++ resources/js/Components/Badge.jsx
@@ -1,6 +1,6 @@
 export default function Badge({ icon: Icon, text, className }) {
     return (
-            <span className={`cursor-pointer inline-flex items-center gap-1 font-semibold text-xs rounded-lg px-2 py-1 mr-3 ${className}`}>
+            <span className={`cursor-pointer inline-flex items-center gap-1 font-semibold text-xs rounded-md px-2 py-1 mr-3 ${className}`}>
                 {Icon && <Icon/>}
                 {text}
             </span>
