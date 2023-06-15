1265fb199e7262a462a1994b3493653b73ffef15
Mohammed Yassin <yssnmed@gmail.com>
2023-06-16 00:51:01 +0100

    working on session details

--- resources/js/Pages/Sessions/Session/Partials/DetailsLayout.jsx
+++ resources/js/Pages/Sessions/Session/Partials/DetailsLayout.jsx
@@ -45,38 +45,65 @@ const DetailsLayout = ({ session }) => {
             </div>
             {/* More info about the user/teacher such as phone number and email address... */}
             {/* Skills */}
-            <div className="px-4 my-4 tags">
-                {JSON.parse(session.tags).skills.map((skill) => {
-                    return (
-                        <Link href={`/?search=${skill}`}>
-                            <Badge
-                            key={skill}
-                            text={skill}
-                            className={`px-3`}
-                            style={{ backgroundColor: randColor() }}
-                        />
-                        </Link>
-                    );
-                })}
+            <div className="px-2 my-4">
+                <h1 className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400  font-black text-m  px-8 mt-8 "> Skills and expertises</h1>
+                <div className="px-4 my-4 tags">
+                    
+                    {JSON.parse(session.tags).skills.map((skill) => {
+                        return (
+                            <Link href={`/?search=${skill}`}>
+                                <Badge
+                                key={skill}
+                                text={skill}
+                                className={`px-3`}
+                                style={{ backgroundColor: randColor() }}
+                            />
+                            </Link>
+                        );
+                    })}
+                </div>
             </div>
             {/* description */}
-            <Card className="p-4 my-4 description">
-                <p className="opacity-[0.8] font-medium text-sm leading-relaxed">
-                    {session.description}...
-                </p>
-            </Card>
+            <div className="px-2 my-4">
+                <h1 className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400  font-black text-m bg-transparent px-8 mt-8 "> Description</h1>
+
+                <Card className="p-4 my-4 description">
+                    <p className="opacity-[0.8] font-medium text-sm leading-relaxed">
+                        {session.description}...
+                    </p>
+                </Card>
+            </div>
+        
             {/* Enrollements */}
             <div className="px-4 mt-8 footer flexible justify-between">
                 <div className="subscribers flexible gap-2">
                     <h4 className="text-sm font-semibold leading-relaxed">
                         {session.enrollments.length >= 1
-                            ? "People enrolled :"
+                            ? <div className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400  font-black text-m  px-8 ">People Enrolled </div>
                             : "No one enrolled"}{" "}
                     </h4>
                     <div className="flex -space-x-2.5">
                         <Avatars users={session.enrollments} />
                     </div>
                 </div>
+            </div>
+
+            {/* Responsibilities : */}
+                  {/* description */}
+            <div className="px-2 my-4">
+                <h1 className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400  font-black text-m  px-8 mt-8 "> Responsibilities</h1>
+                <ul class="max-w-md space-y-1 text-white-500 list-disc list-inside mt-4 ml-8">
+                    <li>
+                        Responsibility number 1
+                    </li>
+                    <li>
+                        Maintaining number 2
+                    </li>
+                    <li>
+                        Staff augmentation
+                    </li>
+                </ul>
+
             </div>
         </div>
     );
